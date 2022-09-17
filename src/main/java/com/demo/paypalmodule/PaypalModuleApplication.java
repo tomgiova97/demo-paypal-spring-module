6a9cebb16ec4fa6e3dfa71e6f6e367a1f9621cfd
Tommaso Giovannelli <giovannelli@strands.com>
2022-09-17 19:10:47 +0200

    First Commit

---
+++ src/main/java/com/demo/paypalmodule/PaypalModuleApplication.java
@@ -0,0 +1,13 @@
+package com.demo.paypalmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalModuleApplication.class, args);
+	}
+
+}
