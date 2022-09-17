668cbf58d7c321eb50f37b71b123136ee5192d34
Tommaso Giovannelli <giovannelli@strands.com>
2022-09-17 19:10:47 +0200

    First Commit

---
+++ src/test/java/com/demo/paypalmodule/PaypalModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.paypalmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
